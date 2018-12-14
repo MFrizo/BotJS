78ec5a10d898a1dcbc7f05f6c10b1c39af1e14e7
MFrizo <matheusfrizo@gmail.com>
2018-12-14 07:23:41 -0200

    All features - v1

--- src/bot.js
+++ src/bot.js
@@ -8,7 +8,19 @@ const client = new Client();
 const path = '../files';
 var fs = require('fs');
 
-var i = 0;
+// Donwload Files Function
+let request = require('request');
+
+function download(url, filename){
+  request.get(url)
+    .on('error', console.error)
+    .pipe(fs.createWriteStream(path+'/'+filename));
+}
+
+// Environment Variables
+var i = false;
+var v = false;
+var n = 1;
 
 // Bot Ready
 client.on('ready', () => {
@@ -17,15 +29,26 @@ client.on('ready', () => {
 
 // Main Core for the Application
 client.on('message', message => {
-  if (i === 0){
+  v = false;
+  
+  if (i === false){
     message.channel.send('Hello, I am your File Bot! How can I help you?');
-    i++;
-  }  
+    i = true;
+  }
+  
+  if (v === false){
+    message.attachments.forEach(function(attachment){
+      download(attachment.url, attachment.filename);
+      message.channel.send('Upload Successful!');
+    });
+  }
 
   if(message.content === 'download files'){
-    message.channel.send(`${message.author}, here are your files available for download!`);
+    v = true;
     var files = fs.readdirSync(path);
 
+    message.channel.send(`${message.author}, here are your files available for download!`);
+    
     files.forEach(function (file){
       const attachment = new Attachment(path+'/'+file);
       message.channel.send(attachment);
